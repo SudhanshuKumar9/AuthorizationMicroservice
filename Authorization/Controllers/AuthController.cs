9eac6fb87635861e987ddc035a876fed7277ae51
Sudhanshu <sud4499@gmail.com>
2020-11-08 14:41:52 +0530

    yaml edited

--- Authorization/Controllers/AuthController.cs
+++ Authorization/Controllers/AuthController.cs
@@ -81,7 +81,7 @@ namespace Authorization.Controllers
                 var encodedData = new StringContent(jsonData, Encoding.UTF8, "application/json");
                 using (var client = new HttpClient())
                 {
-                    var response = client.PostAsync("https://localhost:44355/api/Members/", encodedData);
+                    var response = client.PostAsync("http://52.224.24.195/api/Members/", encodedData);
                     var responseData = response.Result.Content.ReadAsStringAsync();
                     memberDetail = JsonConvert.DeserializeObject<Member>(responseData.Result);
                 }
